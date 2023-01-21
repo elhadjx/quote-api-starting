8df36550fff8c8d231313a450d8255a13ee6cc1f
dinoelhadj <dino.elhadj@gmail.com>
2023-01-21 22:03:07 +0100

    update

--- server.js
+++ server.js
@@ -4,28 +4,22 @@ const app = express();
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'));
 
 app.get('/api/quotes/random', (req, res, next) => {
-    res.send(getRandomElement(quotes))
+    let ee = getRandomElement(quotes)
+    res.status(200).send({quote: ee})
 })
 
 app.get('/api/quotes', (req, res, next) => {
-
     const person = req.params.person;
-    if (person) {
-        let arr = [];
+    let arr = [];
         quotes.forEach(element => {
-            if (element.person == person ){
-                arr.push(element)
-            }
+            arr.push(element)
         });
-        res.send(arr)
-    } else {
-        res.send(quotes)
-    }
+        res.status(200).send({ quotes: arr })
     
 })
 
@@ -35,8 +29,12 @@ app.post('/api/quotes', (req, res, next) => {
     if (q1Q && q1P ){
         let q1 = new Quote(q1Q, q1P)
         quotes.push(q1)
-        res.send(q1)
+        res.status(200).send(q1)
     } else {
         res.status(400).send()
     }
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
