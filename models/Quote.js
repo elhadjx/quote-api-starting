d0328ebeabc5f399b63d12dd3283072f8571c615
dinoelhadj <dino.elhadj@gmail.com>
2023-01-21 19:39:04 +0100

    sdq

---
+++ models/Quote.js
@@ -0,0 +1,15 @@
+class Quote {
+    constructor(quote, person) {
+        this.quote = quote;
+        this.person = person;
+    }
+
+    get quote() {
+        return this.quote;
+    }
+
+    get person() {
+        return this.person;
+    }
+    
+}
--- server.js
+++ server.js
@@ -8,4 +8,35 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.listen(PORT, 'started')
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send(getRandomElement(quotes))
+})
+
+app.get('/api/quotes', (req, res, next) => {
+
+    const person = req.params.person;
+    if (person) {
+        let arr = [];
+        quotes.forEach(element => {
+            if (element.person == person ){
+                arr.push(element)
+            }
+        });
+        res.send(arr)
+    } else {
+        res.send(quotes)
+    }
+    
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    let q1Q = req.params.quote;
+    let q1P = req.params.person;
+    if (q1Q && q1P ){
+        let q1 = new Quote(q1Q, q1P)
+        quotes.push(q1)
+        res.send(q1)
+    } else {
+        res.status(400).send()
+    }
+})
