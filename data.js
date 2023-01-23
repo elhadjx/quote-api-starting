1a7746d4e82d973496554a84f4f39a0d4402a9fc
dinoelhadj <dino.elhadj@gmail.com>
2023-01-23 14:36:25 +0100

    added id to data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const quotes = [
   {
+    id: 1,
     quote: 'We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.',
     person: 'Ellen Ullman'
   },
@@ -8,46 +9,57 @@ const quotes = [
     person: 'Anonymous'
   },
   {
+    id: 2,
     quote: `If it's a good idea, go ahead and do it. It's much easier to apologize than it is to get permission.`,
     person: 'Grace Hopper'
   },
   {
+    id: 3,
     quote: 'The city’s central computer told you?  R2D2, you know better than to trust a strange computer!',
     person: 'C-3PO'
   },
   {
+    id: 4,
     quote: 'I have always wished for my computer to be as easy to use as my telephone; my wish has come true because I can no longer figure out how to use my telephone.',
     person: 'Bjarne Stroustrup'
   },
   {
+    id: 5,
     quote: 'Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand.',
     person: 'Ada Lovelace'
   },
   {
+    id: 6,
     quote: 'Java is to JavaScript as ham is to hamster.',
     person: 'Jeremy Keith'
   },
   {
+    id: 7,
     quote: `The most dangerous phrase in the language is, "We've always done it this way."`,
     person: 'Grace Hopper'
   },
   {
+    id: 8,
     quote: 'As soon as we started programming, we found to our surprise that it wasn’t as easy to get programs right as we had thought.  Debugging had to be discovered.  I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.',
     person: 'Maurice Wilkes'
   },
   {
+    id: 9,
     quote: 'Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.',
     person: 'Bill Gates'
   },
   {
+    id: 10,
     quote: 'What one programmer can do in one month, two programmers can do in two months.',
     person: 'Fred Brooks'
   },
   {
+    id: 11,
     quote: 'The Internet? Is that thing still around?',
     person: 'Homer Simpson'
   },
   {
+    id: 12,
     quote: 'If you tell me precisely what it is a machine cannot do, then I can always make a machine which will do just that.',
     person: 'Jon von Neumann'
   },
